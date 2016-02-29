b0725b7e6b6f41f97bee73cb718ad63fb55c5459
Robert <robert@mediamonks.com>
2016-02-29 16:01:44 +0100

    - User emails are now stored with encryption - Hashed email is automatically stored

--- src/App/CoreBundle/Entity/User.php
+++ src/App/CoreBundle/Entity/User.php
@@ -2,14 +2,15 @@
 
 namespace App\CoreBundle\Entity;
 
-use Sonata\UserBundle\Entity\BaseUser as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use MediaMonks\Doctrine\ORM\Mapping\Annotation as MediaMonks;
 use Symfony\Component\Validator\Constraints as Assert;
+use MediaMonks\Doctrine\Mapping\Annotation as MediaMonks;
+use Zend\Crypt\Hash;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
+ * @ORM\Table(name="users")
  */
 class User extends BaseUser
 {
@@ -20,5 +21,114 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @MediaMonks\Transformable(name="encrypt_symmetric")
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", nullable=true, unique=true)
+     */
+    protected $emailCanonical;
+
+    /**
+     * @ORM\Column(type="string", nullable=true, unique=true)
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", nullable=true, unique=true)
+     */
+    protected $usernameCanonical;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $enabled;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $salt;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $lastLogin;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $confirmationToken;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $passwordRequestedAt;
+
+    /**
+     * @var Collection
+     */
+    protected $groups;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $locked;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $expired;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $expiresAt;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    protected $roles;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $credentialsExpired;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $credentialsExpireAt;
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        $this->setEmailCanonical($email);
+
+        return $this;
+    }
+
+    /**
+     * @param string $emailCanonical
+     * @return $this
+     */
+    public function setEmailCanonical($emailCanonical)
+    {
+        $this->emailCanonical = Hash::compute('sha256', $emailCanonical);
+
+        return $this;
+    }
 
 }
