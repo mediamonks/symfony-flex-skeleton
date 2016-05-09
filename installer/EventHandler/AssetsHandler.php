ddf3abe86fee878f957326566af67460c42960e9
Robert <robert@mediamonks.com>
2016-05-09 15:31:24 +0200

    - Using CommandEvent instead of Event in composer script for installing Symfony assets, this was breaking the installer flow

--- installer/EventHandler/AssetsHandler.php
+++ installer/EventHandler/AssetsHandler.php
@@ -2,6 +2,7 @@
 
 namespace Installer\EventHandler;
 
+use Composer\Script\CommandEvent;
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler;
 
 class AssetsHandler extends AbstractHandler
@@ -10,7 +11,21 @@ class AssetsHandler extends AbstractHandler
     {
         $this->writeHeader('Assets');
 
-        ScriptHandler::installAssets($this->event);
+        try {
+            // Symfony requires the deprecated CommandEvent instead of the regular Event
+            $event = new CommandEvent(
+                $this->event->getName(),
+                $this->event->getComposer(),
+                $this->event->getIO(),
+                $this->event->isDevMode(),
+                $this->event->getArguments(),
+                $this->event->getFlags()
+            );
+            ScriptHandler::installAssets($event);
+        } catch (\Exception $e) {
+            $this->writeError('Could not install assets: ' . $e->getMessage());
+            $this->writeEmpty();
+        }
 
         $this->writeEmpty();
     }
