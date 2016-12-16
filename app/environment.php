3b37bf20795a55a29fc2fe315201bb128f812140
Robert <robertslootjes@gmail.com>
2016-12-16 16:28:29 +0100

    Changed environment names according to new spec Using APCu as default for production, acceptance and testing

--- app/environment.php
+++ app/environment.php
@@ -1,25 +1,27 @@
 <?php
 
+define('ENV_TEST', 'test'); // phpunit / behat
+define('ENV_LOCAL', 'local');
 define('ENV_DEVELOPMENT', 'development');
-define('ENV_TEST', 'test');
-define('ENV_UAT', 'uat');
+define('ENV_TESTING', 'testing');
+define('ENV_ACCEPTANCE', 'acceptance');
 define('ENV_PRODUCTION', 'production');
 
 if (!defined('ENVIRONMENT')) {
     if (($environment = getenv('ENVIRONMENT')) == false || getenv('ENVIRONMENT') == ENV_PRODUCTION) {
         if (php_sapi_name() == 'cli') {
             if (strpos(__DIR__, 'C:') !== false) {
-                $environment = ENV_DEVELOPMENT;
+                $environment = ENV_LOCAL;
             } elseif (strpos(__DIR__, '/Users/') !== false) {
-                $environment = ENV_DEVELOPMENT;
+                $environment = ENV_LOCAL;
             } else {
-                $environment = ENV_PRODUCTION;
+                $environment = ENV_LOCAL;
             }
         } elseif (!empty($_SERVER['SERVER_NAME'])) {
             switch (@$_SERVER['SERVER_NAME']) {
                 case 'localhost':
                 case '127.0.0.1': {
-                    $environment = ENV_DEVELOPMENT;
+                    $environment = ENV_LOCAL;
                     break;
                 }
                 case 'test': {
