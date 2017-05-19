bc0611c9fc3128fc11fd481cb9c5b9c3cf40c312
Eduan de Bruijne <eduan.de.bruijne@mediamonks.com>
2017-05-19 14:44:02 +0200

    Update hostupdate-down.sh script for Mac and Windows

--- tools/vagrant/hostupdate-down.sh
+++ tools/vagrant/hostupdate-down.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 if [ -f "C:/Windows/System32/drivers/etc/hosts" ]
 then
-    cp C:/Windows/System32/drivers/etc/hosts C:/Windows/System32/drivers/etc/hosts_temp
-	sed -i '/'$1'/d' C:/Windows/System32/drivers/etc/hosts_temp
-	cp -f C:/Windows/System32/drivers/etc/hosts_temp C:/Windows/System32/drivers/etc/hosts
+	sed -i '/'$1'/d' C:/Windows/System32/drivers/etc/hosts
 else
-    cp /etc/hosts /etc/hosts_temp
-	sed -i '/'$1'/d' /etc/hosts_temp
-	cp -f /etc/hosts_temp /etc/hosts
+    sed -i .bak -e '/'$1'/d' /etc/hosts
 fi
