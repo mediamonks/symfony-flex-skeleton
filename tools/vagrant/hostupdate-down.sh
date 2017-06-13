90ab98858889826bbdb2859c3ca11c4bd223d432
Eduan de Bruijne <eduan.de.bruijne@mediamonks.com>
2017-06-13 17:09:55 +0200

    Password promt for osx and linux users for host file update

--- tools/vagrant/hostupdate-down.sh
+++ tools/vagrant/hostupdate-down.sh
@@ -3,5 +3,7 @@ if [ -f "C:/Windows/System32/drivers/etc/hosts" ]
 then
 	sed -i '/'$1'/d' C:/Windows/System32/drivers/etc/hosts
 else
-    sed -i .bak -e '/'$1'/d' /etc/hosts
+sudo -i <<EOF
+    sed -i .bak -e '/'$1'/d' "/etc/hosts"
+EOF
 fi
--- tools/vagrant/hostupdate-up.sh
+++ tools/vagrant/hostupdate-up.sh
@@ -3,5 +3,7 @@ if [ -f "C:/Windows/System32/drivers/etc/hosts" ]
 then
 	echo "$1  $2 $3 $4 $5 $6 $7 $8 $9" >> C:/Windows/System32/drivers/etc/hosts
 else
-	echo "$1  $2 $3 $4 $5 $6 $7 $8 $9" >> /etc/hosts
+sudo -i <<EOF
+    echo "$1  $2 $3 $4 $5 $6 $7 $8 $9" >> "/etc/hosts"
+EOF
 fi
