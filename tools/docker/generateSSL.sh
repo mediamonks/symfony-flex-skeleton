59bb70db7f6f7db98994480d38114f1185b452b1
Eduan de Bruijne <eduan.de.bruijne@mediamonks.com>
2017-07-13 16:38:22 +0200

    Revert "Only generate SSL certificate if the certificate does not exists"
    This reverts commit 164718bb6b00975a41c99b3e2a1d43a406738273.

--- tools/docker/generateSSL.sh
+++ tools/docker/generateSSL.sh
@@ -1,38 +1,35 @@
 #!/usr/bin/env bash
-if [ -f ssl.crt ]; then
-   echo SSL.cert already exists
-else
-    HOST=mmsonataextrabundle
-    DOMAIN=lcl
-    IP=192.168.33.173
-    COUNTRY=NL
-    STATE=UT
-    CITY=Hilversum
-    ORGANIZATION=MediaMonks
-    ORGANIZATION_UNIT=PHP
-    EMAIL=info@$HOSTNAME.$DOMAIN
+HOST=symfony-skeleton
+DOMAIN=lcl
+IP=192.168.33.2
+COUNTRY=NL
+STATE=UT
+CITY=Hilversum
+ORGANIZATION=MediaMonks
+ORGANIZATION_UNIT=PHP
+EMAIL=info@$HOSTNAME.$DOMAIN
 
-    (
-    echo [req]
-    echo default_bits = 2048
-    echo prompt = no
-    echo default_md = sha256
-    echo x509_extensions = v3_req
-    echo distinguished_name = dn
-    echo [dn]
-    echo C = $COUNTRY
-    echo ST = $STATE
-    echo L = $CITY
-    echo O = $ORGANIZATION
-    echo OU = $ORGANIZATION_UNIT
-    echo emailAddress = $EMAIL
-    echo CN = $HOST.$DOMAIN
-    echo [v3_req]
-    echo subjectAltName = @alt_names
-    echo [alt_names]
-    echo DNS.1 = $IP
-    echo DNS.2 = $HOST.$DOMAIN
-    )>ssl.cnf
+(
+echo [req]
+echo default_bits = 2048
+echo prompt = no
+echo default_md = sha256
+echo x509_extensions = v3_req
+echo distinguished_name = dn
+echo [dn]
+echo C = $COUNTRY
+echo ST = $STATE
+echo L = $CITY
+echo O = $ORGANIZATION
+echo OU = $ORGANIZATION_UNIT
+echo emailAddress = $EMAIL
+echo CN = $HOST.$DOMAIN
+echo [v3_req]
+echo subjectAltName = @alt_names
+echo [alt_names]
+echo DNS.1 = $IP
+echo DNS.4 = $HOST.$DOMAIN
+echo DNS.5 = *.$HOST.$DOMAIN
+)>ssl.cnf
 
-    openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout ssl.key -days 9000 -out ssl.crt -config ssl.cnf
-fi
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout ssl.key -days 9000 -out ssl.crt -config ssl.cnf
