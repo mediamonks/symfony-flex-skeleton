6f026d49b025de0797196f75384f37e0442c8f19
Eduan de Bruijne <eduan.de.bruijne@mediamonks.com>
2018-05-16 09:31:53 +0200

    Fixes docker scripts

--- tools/docker/phpunit.sh
+++ tools/docker/phpunit.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker exec apache sh -c "cd /var/www/html/deploy && bin/phpunit $1 $2 $3 $4 $5"
+docker exec apache sh -c "cd /var/www/html/source/symfony && bin/phpunit $1 $2 $3 $4 $5"
