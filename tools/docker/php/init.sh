d838a47b33aea5a7225ede6b53172f2aa7547dda
Eduan de Bruijne <eduan.de.bruijne@mediamonks.com>
2019-02-26 15:36:09 +0100

    Removed creating directories for all MM evns, try to solve this with a Composer Plugin

--- tools/docker/php/init.sh
+++ tools/docker/php/init.sh
@@ -13,20 +13,16 @@ cd /var/www/source/symfony; chmod -R 777 var/log var/cache
 # Configuring environments
 #============================================
 if [ -d /var/www/source/symfony/config/packages/local ]; then
-   echo Environments already in place.
+   echo Environments are correct.
 else
     cd /var/www/source/symfony/config/packages
     mv dev local
-    mkdir development
-    mkdir testing
-    mkdir acceptance
-    mv prod production
 
     sed -i 's/APP_ENV=dev/APP_ENV=local/g' /var/www/source/symfony/.env
     sed -i 's/dev/local/g' /var/www/source/symfony/config/bootstrap.php
     sed -i 's/prod/production/g' /var/www/source/symfony/config/bootstrap.php
 
-    echo Environments created.
+    echo Environment dev changed to local and renamed in bootstrap.php
 fi
 
 #
