6bf2969452fc92b8e550d9292c10b522a67b5150
edwin <edwin@mediamonks.com>
2020-07-01 12:34:04 +0200

    Fix init script to set correct environment folders.

--- tools/docker/php/init.sh
+++ tools/docker/php/init.sh
@@ -11,18 +11,26 @@ cd /var/www/source/symfony; chmod -R 777 var/log var/cache
 
 #
 # Configuring environments
+# https://wiki.mediamonks.net/Hosting_Environment_Naming_Convention
 #============================================
 if [ -d /var/www/source/symfony/config/packages/local ]; then
    echo Environments are correct.
 else
     cd /var/www/source/symfony/config/packages
-    mv dev local
+
+    mkdir local
+    sed -i 's/dev/local/g' /var/www/source/symfony/config/bootstrap.php
+
+    if [ -d /var/www/source/symfony/config/packages/prod ]; then
+      mv prod production
+      sed -i 's/prod/production/g' /var/www/source/symfony/config/bootstrap.php
+    fi
+
+    [[ -d /var/www/source/symfony/config/packages/testing ]] || cp -r production testing
 
     sed -i 's/APP_ENV=dev/APP_ENV=local/g' /var/www/source/symfony/.env
-    sed -i 's/dev/local/g' /var/www/source/symfony/config/bootstrap.php
-    sed -i 's/prod/production/g' /var/www/source/symfony/config/bootstrap.php
 
-    echo Environment dev changed to local and renamed in bootstrap.php
+    echo Environment configured.
 fi
 
 #
